c3140911b9d8278dfe460bd95c292c86b065147b
vikramsharitas <43680717+vikramsharitas@users.noreply.github.com>
2019-09-05 12:03:44 +0530

    Bug fixes and minor changes

--- Py2.py
+++ Py2.py
@@ -13,40 +13,87 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 
 day = {
-    'MO' : 0,
-    'TU' : 1,
-    'WE' : 2,
-    'TH' : 3,
-    'FR' : 4,
-    'SA' : 5,
-    'SU' : 6
+    'MO': 0,
+    'TU': 1,
+    'WE': 2,
+    'TH': 3,
+    'FR': 4,
+    'SA': 5,
+    'SU': 6
+}
+month1 = {
+    1: 31,
+    2: 28,
+    3: 31,
+    4: 30,
+    5: 31,
+    6: 30,
+    7: 31,
+    8: 31,
+    9: 30,
+    10: 31,
+    11: 30,
+    12: 31
 }
 
 today = datetime.datetime.today()
-d2 = today.strftime("%Y-%m-%dT")
+d2 = today.strftime("%Y-%m-%d")
 d3 = today.strftime("%Y") + '-08-01T'
-print(d3)
+
+
+def get_date(num):
+    date = int(d2[8:])
+    month = int(d2[5:7])
+    year = int(d2[:4])
+    if date + num > month1[month]:
+        if month == 12:
+            year += 1
+            month = 1
+            date += num - 31
+        elif month == 2:
+            if year % 4 == 0:
+                month += 1
+                date += num - 29
+            else:
+                month += 1
+                date += num - 28
+        else:
+            date += num - month1[month]
+            month += 1
+    else:
+        date += num
+    if month < 10:
+        if date < 10:
+            time1 = str(year) + '-0' + str(month) + '-0' + str(date) + 'T'
+            print(time1)
+            return time1
+        else:
+            time1 = str(year) + '-0' + str(month) + '-' + str(date) + 'T'
+            print(time1)
+            return time1
+    else:
+        if date < 10:
+            time1 = str(year) + '-' + str(month) + '-0' + str(date) + 'T'
+            print(time1)
+            return time1
+        else:
+            time1 = str(year) + '-' + str(month) + '-' + str(date) + 'T'
+            print(time1)
+            return time1
+
 
 def start_date(dates):
     num = day[dates[:2]] - today.weekday()
     if num >= 0:
-        if int(d2[8:10]) < 10:
-            time1 = d2[:8] + '0' + str(int(d2[8:10]) + num) + d2[10:]
-            return time1
-        else:
-            time1 = d2[:8] + str(int(d2[8:10]) + num) + d2[10:]
-            return time1
+        return get_date(num)
     else:
-        num+=7
-        if int(d2[0][8:10]) < 10:
-            time1 = d2[:8] + '0' + str(int(d2[8:10]) + num) + d2[10:]
-            return time1
-        else:
-            time1 = d2[:8] + str(int(d2[8:10]) + num) + d2[10:]
-            return time1
+        num += 7
+        return get_date(num)
+
 
 # If modifying these scopes, delete the file token.pickle.
 SCOPES = ['https://www.googleapis.com/auth/calendar']
+
 
 def get_time(dates, time):
     time1 = ['', '']
@@ -55,23 +102,22 @@ def get_time(dates, time):
     time3 = ''
     for x in time:
         if x == ' ':
-            n+=1
+            n += 1
         if n == 0:
             time3 += x
-    time3 = str(int(time3) + 7)
-    if int(time3) < 10 and (int(time3) + n) < 10:
-        time1[0] = d1 + '0' + time3 + ':00:00'
-        time1[1] = d1 + '0' + str(int(time3) + n) + ':50:00'
+    time3 = (int(time3) + 7)
+    if time3 < 10 and (time3 + n) < 10:
+        time1[0] = d1 + '0' + str(time3) + ':00:00'
+        time1[1] = d1 + '0' + str(time3 + n) + ':50:00'
         return time1
-    elif int(time3) < 10 and int(time3)+n >= 10:
-        time1[0] = d1 + '0' + time3 + ':00:00'
-        time1[1] = d1 + str(int(time3) + n) + ':50:00'
+    elif time3 < 10 and (time3 + n) >= 10:
+        time1[0] = d1 + '0' + str(time3) + ':00:00'
+        time1[1] = d1 + str(time3 + n) + ':50:00'
         return time1
-    elif int(time3) >= 10 and (int(time3) + n) >= 10:
-        time1[0] = d1 + time3 + ':00:00'
-        time1[1] = d1 + str(int(time3) + n) + ':50:00'
+    elif time3 >= 10 and (time3 + n) >= 10:
+        time1[0] = d1 + str(time3) + ':00:00'
+        time1[1] = d1 + str(time3 + n) + ':50:00'
         return time1
-
 
 
 def main():
@@ -131,6 +177,7 @@ def main():
 
                 event = service.events().insert(calendarId='primary', body=event).execute()
     my_course_info.close()
+
 
 if __name__ == '__main__':
     main()
