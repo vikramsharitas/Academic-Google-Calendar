2bc04db0df5d81c32ee1476979a6f9812af63b29
vikramsharitas <43680717+vikramsharitas@users.noreply.github.com>
2019-09-06 16:31:17 +0530

    removed unnecessary lines

--- run_windows.bat
+++ run_windows.bat
@@ -3,7 +3,6 @@ python -m pip install --upgrade pip
 pip install --upgrade selenium
 pip install --upgrade google-api-python-client google-auth-httplib2 google-auth-oauthlib
 pip install --upgrade PyQt5
-pip install --upgrade TimeUnit
 cd /D "%~dp0"
 python get_creds.py
 python down_creds.py
